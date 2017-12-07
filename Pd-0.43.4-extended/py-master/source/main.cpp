e1b022fe06146d03bdd98ca0ca1b0b2ffd1c9525
sylatupa <sylatupa@asu.edu>
2017-12-06 17:01:13 -0700

    new

---
+++ Pd-0.43.4-extended/py-master/source/main.cpp
@@ -0,0 +1,9 @@
+/*
+py/pyext - python external object for PD and MaxMSP
+
+Copyright (c)2002-2015 Thomas Grill (gr@grrrr.org)
+For information on usage and redistribution, and for a DISCLAIMER OF ALL
+WARRANTIES, see the file, "license.txt," in this distribution.  
+*/
+
+#include "pybase.h"
